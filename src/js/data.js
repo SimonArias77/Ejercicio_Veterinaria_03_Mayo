9193f3f34d2660095ccfc92a5c6fa7e952cf37e5
SimonArias77 <sariass077@gmail.com>
2024-05-03 09:23:40 -0500

    se avanza

---
+++ src/js/data.js
@@ -0,0 +1,153 @@
+let mascotas = [
+    {
+        nombre: "Trosky",
+        especie: "Perro",
+        raza: "Cocker Spaniel",
+        edad: 1,
+        peso: 14,
+        estado: "estable",
+        propietario: {
+            nombre: "Daniela Mejìa",
+            documento: "12345999",
+            telefono: "5432144",
+            correo: "dany_mejia@gmail.com"
+        },
+        imagen: "./public/img/dog_trosky.webp"
+    },
+    {
+        nombre: "Lucas",
+        especie: "Perro",
+        raza: "Kangal turco",
+        edad: 4,
+        peso: 65,
+        estado: "crítico",
+        propietario: {
+            nombre: "Mario Guzman",
+            documento: "987",
+            telefono: "987654321",
+            correo: "lmario_guz@gmail.com"
+        },
+        imagen: "./public/img/dog_lucas.webp"
+    },
+    {
+        nombre: "Caos",
+        especie: "Perro",
+        raza: "Pastor Alemàn",
+        edad: 5,
+        peso: 33,
+        estado: "estable",
+        propietario: {
+            nombre: "Simòn Arias",
+            documento: "12345",
+            telefono: "54321",
+            correo: "simon_arias_77@gmail.com"
+        },
+        imagen: "./public/img/dog_caos.webp"
+    },
+    {
+        nombre: "Aròn",
+        especie: "Perro",
+        raza: "Pastor Alemàn",
+        edad: 9,
+        peso: 40,
+        estado: "estable",
+        propietario: {
+            nombre: "Simòn Arias",
+            documento: "12345",
+            telefono: "54321",
+            correo: "simon_arias_77@gmail.com"
+        },
+        imagen: "./public/img/dog_aron.webp"
+    },
+    {
+        nombre: "Scar",
+        especie: "Gato",
+        raza: "Bosque de Noruega",
+        edad: 3,
+        peso: 10,
+        estado: "crìtico",
+        propietario: {
+            nombre: "Daniela Mejìa",
+            documento: "12345999",
+            telefono: "5432144",
+            correo: "dany_mejia@gmail.com"
+        },
+        imagen: "./public/img/cat_scar.webp"
+    },
+    {
+        nombre: "Sombra",
+        especie: "Perro",
+        raza: "Doberman",
+        edad: 4,
+        peso: 42,
+        estado: "crítico",
+        propietario: {
+            nombre: "Benito Reyes",
+            documento: "1234500",
+            telefono: "54321000",
+            correo: "benito_camelas@gmail.com"
+        },
+        imagen: "./public/img/dog_sombra.webp"
+    },
+    {
+        nombre: "Kendra",
+        especie: "Perro",
+        raza: "Mastin Tibetano",
+        edad: 5,
+        peso: 90,
+        estado: "estable",
+        propietario: {
+            nombre: "Germàn González",
+            documento: "987",
+            telefono: "987654321",
+            correo: "ger_gonzalez@gmail.com"
+        },
+        imagen: "./public/img/dog_kendra.webp"
+    },
+    {
+        nombre: "Samy",
+        especie: "Gato",
+        raza: "Bengalí",
+        edad: 3,
+        peso: 4,
+        estado: "crítico",
+        propietario: {
+            nombre: "Elena Martínez",
+            documento: "901234567",
+            telefono: "901234567",
+            correo: "elenamartinez@gmail.com"
+        },
+        imagen: "./public/img/cat_samy.webp"
+    },
+    {
+        nombre: "Emily",
+        especie: "Gato",
+        raza: "Pequinès",
+        edad: 1,
+        peso: 5,
+        estado: "estable",
+        propietario: {
+            nombre: "Mario Pérez",
+            documento: "678901234",
+            telefono: "678901234",
+            correo: "marioperez@gmail.com"
+        },
+        imagen: "./public/img/cat_emily.webp"
+    },
+    {
+        nombre: "Selva",
+        especie: "Perro",
+        raza: "Dogo Argentino",
+        edad: 8,
+        peso: 42,
+        estado: "crítico",
+        propietario: {
+            nombre: "Lucía Rodríguez",
+            documento: "789012345",
+            telefono: "789012345",
+            correo: "luciarodriguez@gmail.com"
+        },
+
+        imagen: "./public/img/dog_selva.webp"
+    }
+]
---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+const main = document.querySelector("main")
+const btnClear = document.querySelector("#btn-clear")
+
+btnClear.addEventListener('click', clearMain)
+
+function index() {
+    main.innerHTML = ""
+    for (const mascota of mascotas) {
+        main.innerHTML += `
+    <article class="card text-center">
+        <img src="${mascota.imagen}" class="card-img-top" alt="${mascota.nombre}"
+            title="${mascota.nombre}">
+        <h2>${mascota.nombre}</h2>
+        <div class="card-body d-flex justify-content-around align-items-center">
+            <div>
+                <p><strong>Especie:</strong> ${mascota.especie}</p>
+                <p><strong>Raza:</strong> ${mascota.raza}</p>
+                <p><strong>Edad:</strong> ${mascota.edad}</p>
+                <p><strong>Peso:</strong> ${mascota.peso}</p>
+                <p><strong>Estado:</strong> ${mascota.estado}</p>
+            </div>
+            <div>
+                <p><strong>Nombre del dueño:</strong> ${mascota.propietario.nombre}</p>
+                <p><strong>Documento del dueño:</strong> ${mascota.propietario.documento}</p>
+                <p><strong>Telefono del dueño:</strong> ${mascota.propietario.telefono}</p>
+                <p><strong>Correo del dueño:</strong> ${mascota.propietario.correo}</p>
+            </div>
+        </div>
+        <div>
+            <a class="btn btn-dark w-25" type="button"> detalles</a>
+        </div>
+    </article>
+    `
+    }
+}
+
+function clearMain() {
+    main.innerHTML = '';
+}
+
+index()
